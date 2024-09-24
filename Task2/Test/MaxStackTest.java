7caff73bfb25d64cf40b51bf09e83f7dcb3bed2b
saadthenexus <hasibulkarimsaad@gmail.com>
2024-09-24 22:02:56 +0600

    Testing push method and max method

---
+++ Task2/Test/MaxStackTest.java
@@ -0,0 +1,20 @@
+package Task2.Test;
+
+import Task2.MaxStack;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MaxStackTest {
+ 
+    @Test
+    public void testPushAndMax() {
+        MaxStack stack = new MaxStack();
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(6);
+        assertEquals(6, stack.max());
+    }   
+}
+
