bffaedab05cefae13d6f70cfbb48c4d8d183f7a1
saadthenexus <hasibulkarimsaad@gmail.com>
2024-09-24 23:14:11 +0600

    Added min method and refactored it that way

--- Task2/MaxStack.java
+++ Task2/MaxStack.java
@@ -2,30 +2,46 @@ package Task2;
 
 import java.util.Stack;
 
+import java.util.Stack;
+
 public class MaxStack {
 
     private Stack<Integer> mainStack;
     private Stack<Integer> maxStack;
+    private Stack<Integer> minStack;
 
-    
+
     public MaxStack() {
         mainStack = new Stack<>();
         maxStack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int x) {
         mainStack.push(x);
+
         if (maxStack.isEmpty() || x >= maxStack.peek()) {
             maxStack.push(x);
         }
+
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
     }
 
+    
     public void pop() {
         if (!mainStack.isEmpty()) {
-            if (mainStack.peek().equals(maxStack.peek())) {
+
+            int poppedValue = mainStack.pop();
+            
+            if (poppedValue == maxStack.peek()) {
                 maxStack.pop();
             }
-            mainStack.pop();
+
+            if (poppedValue == minStack.peek()) {
+                minStack.pop();
+            }
         }
     }
 
@@ -36,4 +52,12 @@ public class MaxStack {
         throw new RuntimeException("Stack is empty");
     }
 
+    public int min() {
+        if (!minStack.isEmpty()) {
+            return minStack.peek();
+        }
+        throw new RuntimeException("Stack is empty");
+    }
+
 }
+
